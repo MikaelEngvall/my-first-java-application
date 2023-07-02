998717ce4bc29f386d7aeb439b3063878e4bd01a
MikaelEngvall <mikael@engvall.org>
2023-07-02 21:33:16 +0200

    my-first-java-application Added iSiTLeap

---
+++ src/main/java/se/lexicon/GUIInput.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+import javax.swing.*;
+
+public class GUIInput {
+    public static void main(String[] args) {
+        double number1 = Double.parseDouble(JOptionPane.showInputDialog("Enter the first number:"));
+        double number2 = Double.parseDouble(JOptionPane.showInputDialog("Enter the second number:"));
+        double diff = number1-number2;
+        double sum = number1+number2;
+        JOptionPane.showMessageDialog(null, "Product: " + number1*number2 + "\nDivision: " + number1/number2 + "\nSum: " + sum + "\nDifference: " + diff);
+    }
+}
